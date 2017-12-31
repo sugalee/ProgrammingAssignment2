f997b7a26ae972bd2f8a15121eafe0b6d49a529f
sugalee <34614113+sugalee@users.noreply.github.com>
2017-12-30 22:58:42 -0600

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
+## makeCacheMatirx function creates a matrix that is 4 by 4 utilizing free variables, y and m. 
+## Then cacheSolve inverses "the matrix created by makeCacheMatrix function using "Solve" function.
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+	makeCacheMatrix <- function(x = matrix()) {
+		m <- NULL
+		set <- function (y) {
+			x <<- y
+			m <<- NULL
+		}
+		get <- function() x
+		setmean <- function (mean) m <<- mean
+		getmean <- function () m
+		matrix (c(set = set, get=get, setmean = setmean, getmean = getmean),
+		nrow=4, 
+		ncol=4)
+	}
+
+
+## Write a short comment describing this function
         ## Return a matrix that is the inverse of 'x'
-}
+	
+	cacheSolve <- function(x, ...) {
+	  	m <- solve (makeCacheMatrix(x))
+		  if (!is.null(m)) {			
+			  	message("getting cached data")
+				  return(m)
+	  	}
+		  data <- x$get()
+		  m <- mean(data,...)
+		  x$setmean(m)
+	  	m
+  }
+
+
